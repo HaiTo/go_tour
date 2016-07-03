8a776eebb8cf662cf95cc244c0fb458b7cd7e4fd
HaiTo <kimura@sansan.com>
2016-07-03 21:13:54 +0900

    new function

--- sandbox.go
+++ sandbox.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Printf("Now you have %g problems.", math.Pi)
+	fmt.Println(add(42, 13))
 }
